7f6a967157db5792b610f026e73b19dbca12bd1a
Andrew Clark <mrandyclark@gmail.com>
2011-01-10 23:03:30 -0700

    firsties

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinyllist::Application.config.secret_token = 'e8445f8c06734717b19a1706071d28646a3f7548c4b24c7afdcbe9660f302e2fd6869def8a05b878e2497c7fb157bca2865e04451512d6d65d795c23d4dc71ff'
